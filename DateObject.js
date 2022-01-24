624f717b9a86f100d33b5cb5cffbde27c572d62f
Nazmul Khan Liton <41168894+nazmulkhanliton@users.noreply.github.com>
2022-01-24 20:21:25 +0600

    Basic

---
+++ DateObject.js
@@ -0,0 +1,14 @@
+var date = new Date();
+console.log(date); //Output 2022-01-24T13:56:41.679Z
+
+var year = date.getFullYear();
+console.log(year); //Output 2022
+
+var month = date.getMonth();
+console.log(month); //Output 0
+
+var currentDate = date.getDate();
+console.log(currentDate); // //Output 24
+
+var currentHour = date.getHours();
+console.log(currentHour); // //Output 20
